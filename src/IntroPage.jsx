33d58db4ca1bd356e020c46bd285f6d4f62b36c5
andreip20 <108472870+andreip20@users.noreply.github.com>
2022-11-15 20:55:30 +0200

    Updated App Name

--- src/IntroPage.jsx
+++ src/IntroPage.jsx
@@ -3,7 +3,7 @@ import "./IntroPage.css";
 export default function IntroPage() {
   return (
     <div className="intro--container">
-      <h1>Quizzlet</h1>
+      <h1>Quizzies</h1>
       <h3>Your to go quizzes!</h3>
     </div>
   );
